985ea99bf0dabae5189dde2da8ed6e58e98e7e2e
jh <jin_hao_chen@163.com>
2019-06-28 23:48:52 +0800

    cjh: 显示 type 和 level in ClubSerializer

--- apps/clubs/serializers.py
+++ apps/clubs/serializers.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+from rest_framework.serializers import Serializer
 from rest_framework.serializers import ModelSerializer
 
 from clubs.models import Club
@@ -7,9 +9,12 @@ from clubs.models import UserClub
 
 class ClubSerializer(ModelSerializer):
 
+    type = serializers.ChoiceField(choices=Club.TYPE_CHOICES, source="get_type_display")
+    level = serializers.ChoiceField(choices=Club.LEVEL_CHOICES, source="get_level_display")
+
     class Meta:
         model = Club
-        fields = ['id', 'name', 'brief', 'icon', 'persons']
+        fields = ['id', 'name', 'brief', 'icon', 'persons', 'type', 'level']
 
 
 class InstituteSerializer(ModelSerializer):
